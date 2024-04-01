d059860e181785ed483c3f8248daa59e3fbdcb70
Victor-Watanabe <watavh@hotmail.com>
2024-04-01 12:02:22 -0300

    update ContatoFacil

--- resultado/views.py
+++ resultado/views.py
@@ -1,5 +1,7 @@
- # Arquivando Valores das Variaáveis
-def result_table (name, address, number, comments):
+import time  # Arquivando Valores das Variaáveis
+
+
+def result_table(name, address, number, comments):
     result_dic = {
         "Resultado": name,
         "Endereço": address,
@@ -8,45 +10,34 @@ def result_table (name, address, number, comments):
     }
     return result_dic
 
- # Imports para o Projeto
+
+# Imports para o Projeto
 from django.shortcuts import render
+from django.http import HttpResponse
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 import pandas as pd
 
 # Rendenizando página HomePage
+selected_phones = []
+
+
 def homepage(request):
     return render(request, 'homepage.html')
 
- # Rendenizando página Search_Page
+
+# Rendenizando página Search_Page
 def search(request):
     return render(request, 'search_page.html')
 
-def select_all(result_dic):
-    contact = []
-    checkbox_all = True
-    html_checkbox = '<input type="checkbox">'
-    javascript_code = """
-       <script>
-       function alterarCheckbox() {
-           var checkbox = document.getElementById('checkbox');
-           checkbox_all = checkbox.checked;
-       }
-       </script>
-       """
-    if checkbox_all == True:
-        contact += result_dic[2]
-    elif():
-        pass
-    return html_checkbox + javascript_code
 
-
-  # Coletando Informações e Apresentando em Tabela
+# Coletando Informações e Apresentando em Tabela
 def all_results(request):
     county = request.POST.get('municipio')
     new_county = county.replace(' ', '+')
@@ -64,7 +55,8 @@ def all_results(request):
     number = []
     comments = []
 
-    url = 'https://www.google.com.br/maps/search/{}+{}/@-24.0416944,-46.5883118,12z/data=!3m1!4b1?entry=ttu'.format(new_local, new_county)
+    url = 'https://www.google.com.br/maps/search/{}+{}/@-24.0416944,-46.5883118,12z/data=!3m1!4b1?entry=ttu'.format(
+        new_local, new_county)
 
     # Abrindo a URL
     driver.get(url)
@@ -104,15 +96,26 @@ def all_results(request):
         if '(' in info:
             number.append(info)
 
+    # Crie uma lista para armazenar o estado do checkbox para cada linha
+    for num in number:
+        number_more_country = '+55 ' + num
+        new_number = number_more_country.replace('(', '').replace(')', '')
+        checkbox_states = (
+            '<div style="display: flex; justify-content: center; transform: scale(1.5);">'
+            f'<input type="checkbox" name="Selecionar" value="{new_number}">'
+            '</div>'
+        )
+
     # Criar um DataFrame a partir do dicionário
     return_dataframe = pd.DataFrame({
         'Nome': name,
         'Endereço': address,
         'Telefone': number,
         'Informações Adicionais': comments,
-        })
+        'Selecionar': checkbox_states
+    })
 
-    return_dataframe['Selecionar'] = return_dataframe.apply(lambda _: '<input type="checkbox">', axis=1)
+    return_dataframe['Telefone'] = return_dataframe['Telefone'].astype(str)
 
     # Convertendo DataFrame para HTML
     result_html = return_dataframe.to_html(index=False, escape=False)
@@ -120,13 +123,67 @@ def all_results(request):
     # Encerrar o driver
     driver.quit()
 
+    contact = number[0]
+    global new_contact
+    new_contact = '+55 ' + contact.replace('(', '').replace(')', '')
+
     return render(request, 'return_page.html', {'result_html': result_html})
 
-def teste_input(request):
-    global teste
-    if request.method == 'POST':
-        input = request.POST.get('message')
-        teste = f"O nome enviado foi: {input}"
-        print(teste)
-    return render(request, ' return_page.html', {'teste':teste})
+    # Automatizando o WhatsApp!
+def auto_whatsapp(request):
+    global new_contact  # Declarando a variável contact como global
 
+    # Configuração Driver.
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+
+    # Adicionando valores as variáveis.
+    url_whatsapp = 'https://web.whatsapp.com/'
+
+    # Se o método da requisição for POST, a variável contact foi definida anteriormente em all_results
+    if request.method == 'POST':
+        message = request.POST.get('message')
+
+        # Abrindo o site utilizando a variável.
+        driver.get(url_whatsapp)
+
+        # Aguardando a página abrir e o usuário adicionar o whatsapp.
+        wait = WebDriverWait(driver, timeout=120)
+
+        # Buscando a opção de nova conversa
+        new_conversation = wait.until(EC.presence_of_element_located((By.XPATH, '//div[@title="Nova conversa"]')))
+        new_conversation.click()
+
+        # Adicionando uma espera global para evitar fechamento do site.
+        driver.implicitly_wait(2)
+
+        # Localizando a barra de Pesquisa e apagando ela.
+        search_bar = driver.find_element(By.XPATH, '//div[@title="Caixa de texto de pesquisa"]')
+        search_bar.send_keys(new_contact)
+        search_bar.send_keys(Keys.CONTROL + 'a')
+        search_bar.send_keys(Keys.DELETE)
+        search_bar.send_keys(new_contact)
+
+        # Localizando o contato e entrando na conversa, adicionando um tempo de espera.
+        contact_search = wait.until(EC.presence_of_element_located((By.XPATH, '//span[@title="{}"]'.format(new_contact))))
+        time.sleep(10)
+        contact_search.click()
+        wait = WebDriverWait(driver, timeout=10)
+
+        # Clickando na barra de mensagem e formatando para enviar a mensagem.
+        message_bar = wait.until(EC.presence_of_element_located((By.XPATH, '//div[@title="Digite uma mensagem"]')))
+        message_bar.send_keys(Keys.CONTROL + 'a')
+        message_bar.send_keys(Keys.DELETE)
+        message_bar.send_keys(message)
+        message_bar.send_keys(Keys.RETURN)
+
+        enviar = driver.find_element(By.XPATH, '//button[@aria-label="Enviar"]')
+        wait = WebDriverWait(driver, timeout=3)
+        enviar.click()
+
+        # Saindo do site após realizar o envio da mensagem solicitada.
+        driver.quit()
+
+        return HttpResponse("Código Realizado com Sucesso.")
+    else:
+        return HttpResponse("Método não suportado")
