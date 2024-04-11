52b0063b5b9f5a3a5a83e9e51d42036ec9d491ed
Victor-Watanabe <watavh@hotmail.com>
2024-04-11 09:57:31 -0300

    Update Screen Beautification

--- resultado/urls.py
+++ resultado/urls.py
@@ -1,11 +1,14 @@
- # Criando URLS padrões para acesso.
+# Criando URLS padrões para acesso.
 from django.urls import path, include
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.homepage, name='home'),
+    path('', views.homepage, name='home'),
     path('search_page/', views.search),
     path('return_page/', views.all_results, name='return_page'),
     path('auto_whatsapp_page/', views.auto_whatsapp, name='auto_whatsapp'),
+    path('timeout_error/', views.auto_whatsapp, name='timeout_error'),
+    path('Error_page/', views.auto_whatsapp, name='ERROR_page'),
 ]
+
